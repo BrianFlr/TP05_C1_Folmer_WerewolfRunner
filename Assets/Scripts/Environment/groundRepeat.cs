e4f0ba9705b5a1a1cba4b2d520a78a2515dd7104
BrianFlr <brianfolmer11@gmail.com>
2026-09-20 00:49:24 -0300

    Ground Repeat y Game Manager
    Creación de Ground Repeat y Game Manager

--- Assets/Scripts/Environment/groundRepeat.cs
+++ Assets/Scripts/Environment/groundRepeat.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class groundRepeat : MonoBehaviour
 {
-    private float groundPositionLoop = 1.6f;
-    private float groundPositionReset = -35f;
+    private BoxCollider2D groundCollider;
+    private float groundWidth = 0f;
+
+    private void Awake()
+    {
+        groundCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Start()
+    {
+        groundWidth = groundCollider.size.x;
+    }
 
     void Update()
     {
         // Si la posicion del suelo es menor al ancho del collider, significa que esta fuera de pantalla
-        if (transform.position.x < groundPositionReset)
+        if (transform.position.x < -groundWidth)
         {
             // Modifico la posicion del suelo y lo teletransporto delante del actual
-            transform.position = new Vector2(groundPositionLoop, -0.85f);
+            transform.Translate(Vector2.left * 2f * groundWidth);
         }
     }
 }
